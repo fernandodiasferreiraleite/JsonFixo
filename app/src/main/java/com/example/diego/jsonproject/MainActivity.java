0edcbdc959df644aa8c6c1e9a39883cc47c13269
dcardosogit <dcardoso86@gmail.com>
2017-03-12 23:34:11 -0300

    Teste de Commit

---
+++ app/src/main/java/com/example/diego/jsonproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.diego.jsonproject;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView textJSON;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textJSON = (TextView) findViewById(R.id.textJSON);
+    }
+
+    public void loadJSON(View v){
+        Album album = Util.convertJSONtoAlbum(Util.rawToJson(getResources().openRawResource(R.raw.teste)));
+        textJSON.setText(album.toString());
+    }
+}
